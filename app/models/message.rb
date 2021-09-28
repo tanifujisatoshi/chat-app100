931b753392f434bffcd4588bc9fdd0284671aded
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-09-29 07:03:53 +0900

    からのメッセージが投稿できないようにバリデーションを設定しました

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ApplicationRecord
 # roomモデルは複数のメッセージを持つことができる
 # userモデルは複数のメッセージを持つことができる
 # それぞれのモデルとmessageモデルは一体多の関係になるのでbelongs_toメソッドを使ってroomモデルとuserモデルの関係を表している
+
+  validates :content, presence: true
 end
