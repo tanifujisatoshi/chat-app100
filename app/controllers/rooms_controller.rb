6b6c5d03b075388d2c2c9d600007a730e8b791d1
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-09-28 13:40:53 +0900

    チャットルーム作成機能の実装について

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,4 +4,24 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 # new.html.erbのform_withメソッド内で新規のroomモデルのインスタンスを使用することで入力した内容がroomsコントローラーのcreateアクションに自動的に送信してくれる
+
+  def create
+    @room = Room.new(room_params)
+# roomモデルの新規インスタンスをroom_paramsの内容を持って生成している
+    if @room.save
+      redirect_to root_path   
+# インスタンスの保存に成功した場合はトップページに遷移させる
+    else
+      render :new
+# インスタンスの保存に失敗した場合は入力した内容が残ったままチャットルーム作成ページに遷移させる
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
+# チャットルームを作成したときにビューからparams(ハッシュ)が送られてくる
+# そのハッシュからまずroomキーを取り出す必要がある nameキーの値の保存を許可している
+# user_idsのバリューは配列であるため配列を受け取るために空の配列を用意している
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,6 @@ class Room < ApplicationRecord
 # roomsテーブルと中間テーブルは一対多の関係になるのでhas_manyオプションで中間テーブルを指定している
 # usersテーブルは多対多の関係であるためhas_manyオプションでusersテーブルを指定している
 # 多対多の関係になったときは間に中間テーブルを置く必要があるのでthroughオプションで中間テーブルを指定している
+  validates :name, presence: true
+# チャットルーム名を空にしたままチャットルームを作成しようとするとエラーが起きる
 end
