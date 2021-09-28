1e3ad3d72260e282cda9d4d8079bef3efd3a8c3f
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-09-28 10:18:03 +0900

    新規登録やログインにて名前を空にさせないようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+# 名前を空にして新規登録やログインが出来ないようにした
+# presenceは存在するという意味
 end
