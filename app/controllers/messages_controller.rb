55782501617c979bdce238250f67c91606c0b9f7
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-09-28 17:55:36 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,15 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+  end
 # メッセージを投稿するためにmessageモデルの新規のインスタンスを生成してform_withのmodelオプションで使えるようにした
 # チャットルーム名をクリックするとメッセージ一覧(messagesコントローラーのindexアクションのビュー)が確認できる
 # そのアクションに遷移するときにmessagesコントローラーのindexアクションにroom_idキーが入ったparamsが届けられる
 # そのキーのバリューを取得するとroomsテーブルのレコードが取得できる
 # form_withの引数にmessageモデルの新規のインスタンスが入った@messageとルームのidが入った@roomを置くことで自動的に入力した内容がmessagesコントローラーのcreateアクションに送信される
-  end
+# チャットルームに紐づいたメッセージを全て取得できる メッセージ一覧で投稿者名を表示するためDBへのアクセス回数が増えるのでincludesメソッドの引数にuserモデルを指定している
+
 
   def create
     @room = Room.find(params[:room_id])
@@ -20,8 +23,11 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
 # メッセージの保存に成功した場合はメッセージ一覧に遷移させる
     else
+      @messages = @room.messages.includes(:user)
       render :index
-# 保存に失敗した場合は入力した情報が残ったまま同じ画面に遷移させる
+# メッセージの保存に失敗した場合は今まで投稿されたメッセージを表示した上でメッセージ一覧に戻すためチャットルームに紐づく今まで投稿されたメッセージを全て取得している
+# 入力された内容を残したままメッセージ一覧に遷移させる
+# indexアクションで@messages = @room.messages.includes(:user)を定義したのでelseの処理に記述しないとエラーが出る
     end
   end
 
